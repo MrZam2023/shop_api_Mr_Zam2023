142f6ac01ab9a77a54900fc8b7b59f86ed6f14fd
MrZam2023 <k.china99@mail.ru>
2023-12-07 14:21:52 +0300

    'HM6'

--- product/urls.py
+++ product/urls.py
@@ -5,9 +5,9 @@ from .views import *
 from users import views as users_views
 
 urlpatterns = [
-    path('categories/', views.category_api_view),
-    path('products/', views.product_api_view),
-    path('products/<int:id>/', views.product_detail_api_view),
-    path('reviews/', views.review_api_view),
-    path('reviews/<int:id>/',views.review_detail_api_view),
+    path('categories/', views.CategoryListAPIView.as_view()),
+    path('products/', views.ProductListAPIView.as_view()),
+    path('products/<int:id>/', views.ProductDetailAPIView.as_view()),
+    path('reviews/', views.ReviewListAPIView.as_view()),
+    path('reviews/<int:id>/',views.ReviewDetailAPIView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -3,7 +3,106 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Product, Category, Review, Tag
 from .serializers import ProductSerializer, CategorySerializer, ReviewSerializer, TagSerializer, ProductValidateSerializer, CategoryValidateSerializer, ReviewValidateSerializer, TagValidateSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.pagination import PageNumberPagination
 
+
+
+class ProductListAPIView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = PageNumberPagination
+    def create(self, request, *args, **kwargs):
+        serializer = ProductValidateSerializer
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE, data=serializer.errors)
+        title = request.data.get('title')
+        description = request.data.get('description')
+        price = request.data.get('price')
+        category_id = request.data.get('category_id')
+
+        product = Product.objects.create(
+            title=title, description=description, price=price, category_id=category_id
+        )
+        return Response(data={'product_id': product.id}, status=status.HTTP_201_CREATED)
+
+
+class ProductDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = PageNumberPagination
+
+
+
+class CategoryListAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
+
+    def create(self, request, *args, **kwargs):
+        serializer = CategoryValidateSerializer
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE, data=serializer.errors)
+        category_list = Category.objects.all()
+        data = CategorySerializer(instance=category_list, many=True).data
+        return Response(data=data)
+
+
+        name = request.data.get('name')
+        category = Category.objects.create(
+            name=name
+     )
+        return Response(data={'category_id': category.id}, status=status.HTTP_201_CREATED)
+
+class CategoryDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
+    lookup_field = 'id'
+
+class ReviewListAPIView(ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    pagination_class = PageNumberPagination
+    def create(self, request, *args, **kwargs):
+        serializer = ReviewValidateSerializer
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE, data=serializer.errors)
+        text = request.data.get('text')
+        stars = request.data.get('stars')
+        product_id = request.data.get('product_id')
+
+        review = Review.objects.create(
+            text=text, stars=stars, product_id=product_id
+        )
+        return Response(data={'review_id': review.id}, status=status.HTTP_201_CREATED)
+
+class ReviewDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    pagination_class = PageNumberPagination
+    lookup_field = 'id'
+
+class TagListAPIView(ListCreateAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = PageNumberPagination
+    def create(self, request, *args, **kwargs):
+        serializer = TagValidateSerializer
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE, data=serializer.errors)
+        name = request.data.get('name')
+
+        tag = Tag.objects.create(
+            name=name
+        )
+        return Response(data={'tag_id': tag.id}, status=status.HTTP_201_CREATED)
+
+class TagDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = PageNumberPagination
+    lookup_field = 'id'
 
 @api_view(['GET', 'POST'])
 def product_api_view(request):
